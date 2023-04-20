b91d17390e978d773a6f2e6e84260754d33b831c
starprince7 <princeagezinweke@gmail.com>
2023-04-20 10:24:34 +0100

    config: Production configuration setup.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -22,13 +22,13 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
                     <View
                         style={tw`bg-transparent flex-row items-center rounded-lg shadow-lg -ml-5 mr-0.5`}
                     >
-                        <MaterialIcons size={15} name='ac-unit' style={tw`mr-0.5 ml-4.5 text-[#A9AFA7] dark:text-gray-200`} />
+                        <MaterialIcons size={15} name='shopping-basket' style={tw`mr-0.5 ml-4.5 text-[#A9AFA7] dark:text-gray-200`} />
                         <Text
                             lightColor="black"
                             darkColor="white"
-                            style={tw`font-extrabold uppercase text-[#A9AFA7] text-[13px]`}
+                            style={tw`font-extrabold text-[#A9AFA7] text-[15px]`}
                         >
-                            Viss Store
+                            VissStore
                         </Text>
                     </View>
                     <View style={tw`flex flex-row bg-transparent pt-1`}>
